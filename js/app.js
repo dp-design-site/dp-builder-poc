faf558b8e92a9101f55147e892b3ae32f7c72d69
dp-design-site <danypavlov89@gmail.com>
2025-08-14 23:06:17 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-<script type="module" id="__inline_js_do_not_edit">
+
 import { initCanvas, createFromPalette, serialize, deserialize, alignAPI, setSelection } from './js/core/canvas.js';
 import { buildPalette } from './js/core/widgets.js';
 import { initInspector } from './js/core/inspector.js';
+
+<script type="module" id="__inline_js_do_not_edit">
 
 // Palette
 buildPalette(document.getElementById('palette'), type => createFromPalette(type));
